26b45f46357f4ba4dc16eaed2fbcab2733d2e4d3
Programador7 <programador7@olan.edu.co>
2024-09-13 16:09:15 -0500

    add enviroment variable

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,16 +1,52 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_typescript_1 = require("sequelize-typescript");
 const productos_1 = require("../model/productos");
 const transferencias_1 = require("../model/transferencias");
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+const dialectOptions = process.env.NODE_ENV !== 'development'
+    ? {
+        postgres: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Permitir certificados autofirmados
+            }
+        }
+    }
+    : {};
 const sequelize = new sequelize_typescript_1.Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: '123',
-    port: 5433,
-    database: 'backend-api',
+    dialectOptions: dialectOptions['postgres'] || {},
+    dialect: process.env.DIALECT,
+    host: process.env.HOST,
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    port: parseInt(process.env.PORT || '8080', 10),
+    database: process.env.DATABASE,
     models: [productos_1.Producto, transferencias_1.Transferencia],
-    logging: false,
+    logging: true,
 });
 exports.default = sequelize;
