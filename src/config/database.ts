a82c5b19a12d460f145c250782d5ea08eaf868df
Programador7 <programador7@olan.edu.co>
2024-09-13 15:43:11 -0500

    rename connection database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,17 @@
 import { Sequelize } from 'sequelize-typescript';
 import { Producto } from '../model/productos';
-import {Transferencia} from '../model/transferencias'
+import { Transferencia } from '../model/transferencias';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const sequelize = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: '123',
-  port: 5433,
-  database: 'backend-api',
+  dialect: process.env.DIALECT as 'postgres',  
+  host: process.env.HOST,
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  port: parseInt(process.env.PORT || '8080', 10),  
+  database: process.env.DATABASE,
   models: [Producto, Transferencia],
   logging: false,
 });
